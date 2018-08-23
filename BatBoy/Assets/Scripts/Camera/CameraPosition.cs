78688cd38370ed54337f806f77ebd48592c4c779
Unknown <ale.bros@hotmail.com>
2018-08-23 17:58:01 -0300

    New Commit

---
+++ BatBoy/Assets/Scripts/Camera/CameraPosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+
+    Vector3 mousePos;
+
+    void Update()
+    {
+        /*mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 vec = player.transform.position - mousePos;
+        vec.z = 0;
+        if(vec.magnitude < 4 && vec.magnitude > 1)
+        {
+            vec /= 2;
+            transform.position = mousePos + vec;
+        }*/
+    }
+}
