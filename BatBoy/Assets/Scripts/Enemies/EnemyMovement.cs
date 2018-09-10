6b1c395fdd693f6f936afbf0475a4306e824fa9c
Unknown <ale.bros@hotmail.com>
2018-09-10 16:00:04 -0300

    v0.1.3
    - Enemy1 prefab modified.
    - Enemy1 now moves with Rigidbody.
    - Enemy1 needs to be moved following the Player.

---
+++ BatBoy/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Health))]
+public class EnemyMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    GameObject target;
+
+    [SerializeField] float speed;
+    [SerializeField] float distanceMin;
+    [SerializeField] float distanceMax;
+
+    Health health;
+
+    void Awake()
+    {
+        target = GameObject.Find("Player");
+    }
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector3 diff = target.transform.position - transform.position;
+        diff.z = 0;
+        Vector3 dir = diff.normalized;
+        float dist = diff.magnitude;
+
+
+        if (dist < distanceMax)
+        {
+            if (dist > distanceMin)
+            {
+                Vector2 vecForce;
+                vecForce.x = Input.GetAxis("Horizontal") * speed;
+                vecForce.y = Input.GetAxis("Vertical") * speed;
+                rb.AddForce(vecForce);
+            }
+            else
+            {
+                // Acá atacaría
+            }
+        }
+    }
+
+    public void Damaged(int dmg)
+    {
+        health.Amount -= dmg;
+    }
+}
