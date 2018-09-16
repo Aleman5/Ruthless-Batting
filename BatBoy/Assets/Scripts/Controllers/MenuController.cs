26d5eb3941fff935507e47586d8e2230cd3e3835
Matias <ruiztorresmatias@gmail.com>
2018-09-16 13:09:24 -0300

    v0.2.12
    -Menu scene added to the project
    -MenuController added to the project

---
+++ BatBoy/Assets/Scripts/Controllers/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    private void Update()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void Play()
+    {
+        StartCoroutine(ChangeScene());
+    }
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+    IEnumerator ChangeScene()
+    {
+        //float fadeTime = GameObject.Find("Fade").GetComponent<Fading>().BeginFade(1);
+        //yield return new WaitForSeconds(fadeTime);
+        yield return new WaitForSeconds(0.2f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
