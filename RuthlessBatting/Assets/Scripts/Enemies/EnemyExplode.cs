1fb15a1b063caba5e7e19db3ff47f2a987fd02ed
Matias <ruiztorresmatias@gmail.com>
2018-10-15 18:35:36 -0300

    Commit #52
    - Enemy movement with navmesh configured (no sliding
    - Enemy Bomb configured (timer)

--- RuthlessBatting/Assets/Scripts/Enemies/EnemyExplode.cs
+++ RuthlessBatting/Assets/Scripts/Enemies/EnemyExplode.cs
@@ -6,6 +6,7 @@ public class EnemyExplode : MonoBehaviour {
 
     [SerializeField] EnemyMovNav enemyMovement;
     [SerializeField] Health enemyHealth;
+    [SerializeField] float timeToExplode;
     bool isRunning;
 
     SphereCollider sphere;
@@ -31,7 +32,7 @@ public class EnemyExplode : MonoBehaviour {
         enemyMovement.IsStop();
         enemyMovement.enabled = false;
 
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(timeToExplode);
 
         ChangeBoxState();
 
