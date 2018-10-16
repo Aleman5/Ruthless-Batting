e1f7ee82730e3084b0a580f48d11ff21fe7abd94
Matias <ruiztorresmatias@gmail.com>
2018-10-16 19:57:46 -0300

    Commit #57
    - New Menu implemented with the game logo.
    - Fade in for the logo configured

---
+++ RuthlessBatting/Assets/Scripts/Canvas/Fade.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour {
+
+    [SerializeField] float fadeRate;
+    private Image image;
+    private float difference = 0.1f;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    void FixedUpdate()
+    {
+        FadeIn();
+    }
+
+    void FadeIn()
+    {
+            fadeRate += difference * Time.deltaTime;
+            if (fadeRate < 0)
+                fadeRate = 0;
+            image.color = new Color(255, 255, 255, fadeRate);
+    }
+}
