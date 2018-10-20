0eee07c357d90590b1ea6fa06dd3d6dcfa81074b
Matias <ruiztorresmatias@gmail.com>
2018-10-20 17:38:18 -0300

    Commit #79
    - Visual indicator for attack added

---
+++ RuthlessBatting/Assets/Scripts/General/MouseDirection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDirection : MonoBehaviour {
+
+    [SerializeField] Transform obj;
+
+    void Update()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, obj.transform.position.y));
+        Vector3 forward = mouseWorld - obj.transform.position;
+        obj.transform.rotation = Quaternion.LookRotation(forward, Vector3.up);
+    }
+}
---
+++ RuthlessBatting/Assets/Scripts/Player/PlayerAimDirection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAimDirection : MonoBehaviour
+{
+
+    [SerializeField] Transform player;
+    [SerializeField] float length;
+
+    void Start()
+    {
+        player = transform.parent.transform;
+    }
+
+    void Update()
+    {
+        Vector3 playerToMouseDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - player.position;
+        playerToMouseDir.z = 0;
+        playerToMouseDir.y = 0;
+
+        Debug.Log(playerToMouseDir);
+
+        transform.position = player.position + (length * playerToMouseDir.normalized);
+    }
+}
