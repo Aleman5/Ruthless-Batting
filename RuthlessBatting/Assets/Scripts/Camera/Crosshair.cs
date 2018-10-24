64d183e11e89f69df37ff6aa630d83a3652a2534
Matias <ruiztorresmatias@gmail.com>
2018-10-23 21:46:19 -0300

    Commit #82
    - Crosshair animation configured in the scene
    - Crosshair script configured
    - Animations for Crosshair created

---
+++ RuthlessBatting/Assets/Scripts/Camera/Crosshair.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour {
+
+    [SerializeField] Canvas myCanvas;
+
+	void Start () {
+        Cursor.visible = false;
+	}
+
+	void Update () {
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(myCanvas.transform as RectTransform, Input.mousePosition, myCanvas.worldCamera, out pos);
+        transform.position = myCanvas.transform.TransformPoint(pos);
+    }
+
+}
