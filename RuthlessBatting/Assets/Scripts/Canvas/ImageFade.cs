fc42e012dff01b1d0faf79e025a6961c0f036b45
Alejandro <ale.bros@hotmail.com>
2018-11-04 14:22:42 -0300

    Commit #117
    - Added Storyboard scripts.

---
+++ RuthlessBatting/Assets/Scripts/Canvas/ImageFade.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageFade : MonoBehaviour
+{
+
+    [SerializeField] float fadeRate;
+    [SerializeField] float timeBeingTheKing;
+
+    private Image image;
+    private float difference = 0.1f;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    void FixedUpdate()
+    {
+        FadeIn();
+    }
+
+    void FadeIn()
+    {
+        fadeRate += difference * Time.deltaTime;
+        if (fadeRate < 0)
+            fadeRate = 0;
+        image.color = new Color(255, 255, 255, fadeRate);
+    }
+}
