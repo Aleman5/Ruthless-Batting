d6bd98464c1766af330ef2ae2b6b48bac209c4e4
Alejandro <ale.bros@hotmail.com>
2019-01-05 20:15:03 -0300

    Commit #180
    - Message appears when Shinu tries to leave the house (Level 1).

---
+++ RuthlessBatting/Assets/Scripts/General/ShowTextOnTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowTextOnTrigger : MonoBehaviour
+{
+    SpriteRenderer spr;
+
+    void Awake()
+    {
+        spr = GetComponentInChildren<SpriteRenderer>();
+        spr.enabled = false;
+    }
+
+    void OnCollisionEnter()
+    {
+        spr.enabled = true;
+    }
+
+    void OnCollisionExit()
+    {
+        spr.enabled = false;
+    }
+}
