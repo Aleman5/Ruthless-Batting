b426eff0d8a121ae0f2d845b980f51c4182fdd43
Alejandro <ale.bros@hotmail.com>
2019-02-20 19:52:47 -0300

    Commit #281
    - Player walks slowly when passing through the stairs in Level 3.

---
+++ RuthlessBatting/Assets/Scripts/General/StairSlowDown.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairSlowDown : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.transform.parent.CompareTag("Player"))
+            col.transform.parent.GetComponent<PlayerMovement3D>().IsOnStairs = true;
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.transform.parent.CompareTag("Player"))
+            col.transform.parent.GetComponent<PlayerMovement3D>().IsOnStairs = false;
+    }
+}
