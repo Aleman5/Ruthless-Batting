f4a0244c3c0736d875a984b560f0504161c31b52
Alejandro <ale.bros@hotmail.com>
2019-02-19 21:54:36 -0300

    Bug Fix #39
    - Multiplier text is behind Wall.

--- RuthlessBatting/Assets/Scripts/General/MultiplierTextSettings.cs
+++ RuthlessBatting/Assets/Scripts/General/MultiplierTextSettings.cs
@@ -4,6 +4,16 @@ public class MultiplierTextSettings : MonoBehaviour
 {
     [SerializeField] float speed;
 
+    const string sortingOrderName = "Wall ChangeUp";
+    const int sortingOrder = 2;
+
+    void Awake()
+    {
+        MeshRenderer mR = GetComponent<MeshRenderer>();
+        mR.sortingLayerName = sortingOrderName;
+        mR.sortingOrder = sortingOrder;
+    }
+
     void Update()
     {
         Vector3 mov = new Vector3(0.0f, speed * Time.deltaTime, 0.0f);
