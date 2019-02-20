73f95e101a5c2ac3fad35e4b3904989aec59816e
Matias <ruiztorresmatias@gmail.com>
2019-02-20 11:02:59 -0300

    Commit #277
    - Grenade explotion configured

---
+++ RuthlessBatting/Assets/Scripts/Weapons/GrenadeAnimation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeAnimation : MonoBehaviour {
+
+    Animator anim;
+
+    void Start () {
+        anim = GetComponentInChildren<Animator>();
+        anim.SetBool("explode", true);
+    }
+}
