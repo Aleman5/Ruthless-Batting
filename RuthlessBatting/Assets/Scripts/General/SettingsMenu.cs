32cc0f4393e26ffe3c90bc1c1d97acc866b34fa3
Matias <ruiztorresmatias@gmail.com>
2019-02-25 16:23:00 -0300

    Commit #307
    - Resolutions now displays hz for more displays

--- RuthlessBatting/Assets/Scripts/General/SettingsMenu.cs
+++ RuthlessBatting/Assets/Scripts/General/SettingsMenu.cs
@@ -71,7 +71,7 @@ public class SettingsMenu : MonoBehaviour {
         int currentResolutionIndex = 0;
         for (int i = 0; i < resolutions.Length; i++)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
+            string option = resolutions[i].width + " x " + resolutions[i].height + " - " +resolutions[i].refreshRate + "Hz";
             options.Add(option);
 
             if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
